22b2ba4ccace92b5a256d9f362e7cef1f66729cf
SKY1GER <saikumarb9119@gmail.com>
2025-06-14 12:40:52 +0530

    expense-ansible-roles

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 component=$1
 env=$2
+app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/SKY1GER/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$env
+ansible-pull -i localhost, -U https://github.com/SKY1GER/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$env -e appVersion=$app_Version
